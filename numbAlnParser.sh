18f595877ddd8c32f4f365a66ce8069401f5c817
Alan Lorenzetti <alan.lorenzetti@gmail.com>
2020-04-02 19:58:14 -0700

    Automatic commit

---
+++ numbAlnParser.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+kmers="15 17 19 21 23 25 27"
+prefixes=`ls $rawdir/*.fastq.gz | sed 's/^raw\///;s/_R[12].*$//' | sort | uniq`
+
+for k in $kmers ; do
+  for p in $prefixes ; do
+    line=`grep '\[quant\] processed' output${k}/${p}/kallistoQuant.log`
+    total=`echo $line | perl -pe 's/^.*processed (.*) reads,.*$/\1/;s/,//g'`
+    psaln=`echo $line | perl -pe 's/^.*reads, (.*) reads pseudoaligned.*$/\1/;s/,//g'`
+    pct=`echo $psaln/$total | bc`
+
+    lineTrimmed=`grep '\[quant\] processed' outputTrimmed$k/${p}/kallistoQuant.log`
+    totalTrimmed=`echo $line | perl -pe 's/^.*processed (.*) reads,.*$/\1/;s/,//g'`
+    psalnTrimmed=`echo $line | perl -pe 's/^.*reads, (.*) reads pseudoaligned.*$/\1/;s/,//g'`
+    pctTrimmed=`echo $psalnTrimmed/$totalTrimmed | bc`
+
+    echo $total $psaln $pct $totalTrimmed $psalnTrimmed $pctTrimmed
+  done
+done
