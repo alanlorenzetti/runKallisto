ce9ea94e23fb15aeb68c64880725343bce7e3a21
Alan Lorenzetti <alan.lorenzetti@gmail.com>
2020-05-19 16:26:48 -0700

    adding script to run variable index tests

---
+++ runKallistoIterations.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+for k in 11 13 15 17 19 21 23 25 27 ; do bash run-kallisto.sh -t 4 -o outputTrimmed$k -g ../misc/Hsalinarum-gene-annotation-pfeiffer2019.gff3 -i ../misc/Hsalinarum-IS-annotation-intact-pfeiffer2019.gff3 -G ../misc/Hsalinarum.fa -a ../misc/adap.fa -s y -p n -k $k -T y > run-kallistoTrimmed$k.log 2> run-kallistoTrimmed$k.err; done
+for k in 11 13 15 17 19 21 23 25 27 ; do bash run-kallisto.sh -t 4 -o output$k -g ../misc/Hsalinarum-gene-annotation-pfeiffer2019.gff3 -i ../misc/Hsalinarum-IS-annotation-intact-pfeiffer2019.gff3 -G ../misc/Hsalinarum.fa -a ../misc/adap.fa -s y -p n -k $k -T n > run-kallisto$k.log 2> run-kallisto$k.err; done
