86e001cd504a1fe4575ad6a1edefa442baccfe3d
Alan Lorenzetti <alan.lorenzetti@gmail.com>
2021-06-27 00:03:55 -0300

    Updated log file destination

--- runKallistoIterations.sh
+++ runKallistoIterations.sh
@@ -6,7 +6,7 @@ for k in 11 13 15 17 19 21 23 25 27 ; do bash run-kallisto.sh -t 12 -o outputTri
 for k in 11 13 15 17 19 21 23 25 27 ; do bash run-kallisto.sh -t 12 -o output$k -g ../misc/Hsalinarum-gene-annotation-pfeiffer2019.gff3 -i ../misc/Hsalinarum-IS-annotation-intact-pfeiffer2019.gff3 -G ~/dlsm/misc/Hsalinarum_nrtx.fa -x n -a ../misc/adap.fa -s n -p y -k $k -T n -B n > run-kallisto$k.log 2> run-kallisto$k.err; done
 
 # generating number of aligned reads for each tested approach 
-bash scripts/numbAlnParser.sh
+bash scripts/numbAlnParser.sh > results/numbAlnParser.log 2>&1
 
 # generating plots
 R -q -f scripts/plotKmerTests.R > results/plotKmerTests.log 2>&1
