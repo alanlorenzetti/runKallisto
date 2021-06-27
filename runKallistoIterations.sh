d1357fcd2275f91d6495feaa5d087e2bf388c4bd
Alan Lorenzetti <alan.lorenzetti@gmail.com>
2021-06-27 00:10:15 -0300

    Updated paths

--- runKallistoIterations.sh
+++ runKallistoIterations.sh
@@ -2,8 +2,8 @@
 
 # running iterations for trimmed and non trimmed analyses
 # in order to check the best kmer size of kallisto
-for k in 11 13 15 17 19 21 23 25 27 ; do bash run-kallisto.sh -t 12 -o outputTrimmed$k -g ../misc/Hsalinarum-gene-annotation-pfeiffer2019.gff3 -i ../misc/Hsalinarum-IS-annotation-intact-pfeiffer2019.gff3 -G ~/dlsm/misc/Hsalinarum_nrtx.fa -x n -a ../misc/adap.fa -s n -p y -k $k -T y -B n > run-kallistoTrimmed$k.log 2> run-kallistoTrimmed$k.err; done
-for k in 11 13 15 17 19 21 23 25 27 ; do bash run-kallisto.sh -t 12 -o output$k -g ../misc/Hsalinarum-gene-annotation-pfeiffer2019.gff3 -i ../misc/Hsalinarum-IS-annotation-intact-pfeiffer2019.gff3 -G ~/dlsm/misc/Hsalinarum_nrtx.fa -x n -a ../misc/adap.fa -s n -p y -k $k -T n -B n > run-kallisto$k.log 2> run-kallisto$k.err; done
+for k in 11 13 15 17 19 21 23 25 27 ; do bash run-kallisto.sh -t 12 -o outputTrimmed$k -g ../misc/Hsalinarum-gene-annotation-pfeiffer2019.gff3 -i ../misc/Hsalinarum-IS-annotation-intact-pfeiffer2019.gff3 -G ../misc/Hsalinarum_nrtx.fa -x n -a ../misc/adap.fa -s n -p y -k $k -T y -B n > run-kallistoTrimmed$k.log 2> run-kallistoTrimmed$k.err; done
+for k in 11 13 15 17 19 21 23 25 27 ; do bash run-kallisto.sh -t 12 -o output$k -g ../misc/Hsalinarum-gene-annotation-pfeiffer2019.gff3 -i ../misc/Hsalinarum-IS-annotation-intact-pfeiffer2019.gff3 -G ../misc/Hsalinarum_nrtx.fa -x n -a ../misc/adap.fa -s n -p y -k $k -T n -B n > run-kallisto$k.log 2> run-kallisto$k.err; done
 
 # generating number of aligned reads for each tested approach 
 bash scripts/numbAlnParser.sh > results/numbAlnParser.log 2>&1
